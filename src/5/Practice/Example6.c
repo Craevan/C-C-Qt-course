b85a9dbf1073876de1250e048f1f2cb47ab4d83d
crevan <a.siverskii@gmail.com>
2019-11-02 14:17:23 +0300

    add Example6 lecture 5

---
+++ src/5/Practice/Example6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][2] = {2321, 3214,
+                   23, 25,
+                   52, 3425};
+
+    int *pi = 13;
+    int *pi2 = &a[1][1];
+    int **pipi = &pi;
+    pi = pi2;
+    printf("%d\n", *pipi);
+
+//    pi = pi + 1; // работаем c указателем
+//
+//    printf("%p\n", *pi);
+//    printf("%d\n", *pi);
+//    printf("%d\n", pi);
+
+    return 0;
+}
