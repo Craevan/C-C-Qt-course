8270f6b432d840ca9cd9d497f1f5e6f264a45b50
Crevan <a.siverskii@gmail.com>
2019-12-17 15:50:15 +0300

    Начало разработки сервера под морской бой.

---
+++ src/HomeWorks/SeaBattle/SRV/field.cpp
@@ -0,0 +1,21 @@
+#include "field.h"
+
+void Field::fieldInitialization(const QString &string)
+{
+
+}
+
+Cell Field::getCell(int x, int y)
+{
+
+}
+
+void Field::setCell(int x, int y)
+{
+
+}
+
+bool Field::isCorrect(const QString &string)
+{
+
+}
